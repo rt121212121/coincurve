e21f572a4d52f25b634b9493abe01e82684b6d36
Roger Taylor <roger.taylor.email@gmail.com>
2019-03-06 21:58:00 +1300

    Test in CI

--- .azure-pipelines/build_windows_wheels.sh
+++ .azure-pipelines/build_windows_wheels.sh
@@ -12,19 +12,19 @@ build_dll() {
 # Step up above the git repo checkout directory.
 pushd ..
 git clone https://github.com/electrumsv/secp256k1
-# Keep synchronised with 'setup.py' located commit used by Linux/MacOS.
-git checkout 314a61d72474aa29ff4afba8472553ad91d88e9d
 
 if [[ "${LINUX_TARGET}" == "win32" ]]; then
-    mv secp256k1 32bit
-    cd 32bit
+    cd secp256k1
+    # Keep synchronised with 'setup.py' located commit used by Linux/MacOS.
+    git checkout 314a61d72474aa29ff4afba8472553ad91d88e9d
     build_dll i686-w64-mingw32
     mv .libs/libsecp256k1-0.dll ../clean/electrumsv_secp256k1/libsecp256k1.dll
     cd ../clean
     python setup.py bdist_wheel --universal --plat-name=win32
 elif [[ "${LINUX_TARGET}" == "win64" ]]; then
-    mv secp256k1 64bit
-    cd 64bit
+    cd secp256k1
+    # Keep synchronised with 'setup.py' located commit used by Linux/MacOS.
+    git checkout 314a61d72474aa29ff4afba8472553ad91d88e9d
     build_dll x86_64-w64-mingw32
     mv .libs/libsecp256k1-0.dll ../clean/electrumsv_secp256k1/libsecp256k1.dll
     cd ../clean
